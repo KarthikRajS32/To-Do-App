920366841a2fb4f5da0df2b19aa60ffc774f916d
KarthikRajS32 <karthikrajit32@gmail.com>
2025-09-22 12:41:16 +0530

    update

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
 var btn = document.getElementById("btn-container");
-if(task.value == ""){
-    btn.hidden = true;
+if (task.value == "") {
+  btn.hidden = true;
 }
-let numbtn=0;
+let numbtn = 0;
 
 function addtask() {
   var task = document.getElementById("task");
   var del = document.getElementById("btn-del");
   var add = document.getElementById("add");
   var btn = document.getElementById("btn-container");
-  var list = document.getElementById("list");
 
   if (task.value == "") {
     alert("Please Enter Your Task");
+    btn.hidden = true; 
     return;
-    btn.hidden = true;
   }
 
-if (task.value) {
-    var ul = document.createElement("ul");
-     let taskId = `btn_${numbtn}`;
-       add.innerHTML += `
-                <ul class="ul" id="${taskId}">
-                    ${task.value}
-                    <button class="btn1" onclick="deltask('${taskId}')">Done</button>
-                </ul>
-            `;
+  if (task.value) {
+    let taskId = `btn_${numbtn}`;
+    add.innerHTML += `
+            <ul class="ul" id="${taskId}">
+                <span id="value">${task.value}</span> 
+                <button class="btn1" onclick="edittask('${taskId}')">Edit</button>
+                <button class="btn1" onclick="deltask('${taskId}')">Done</button>
+            </ul>
+        `;
     numbtn++;
     task.value = "";
   }
-  del.addEventListener("click", () => {
-    add.innerHTML = "";
-    btn.hidden = true;
-  });
-
 }
 
-task.addEventListener("keypress",(event)=>{
-  if(event.key === "Enter"){
+task.addEventListener("keypress", (event) => {
+  if (event.key === "Enter") {
     addtask();
   }
-})
-
+});
 
 function deltask(id) {
   var list = document.getElementById(id);
-  if(list){
+  if (list) {
     list.remove();
   }
+}
+
+function edittask(taskId){
+  var id = document.getElementById(taskId);
+  var value = document.getElementById("value");
+ 
+  if (!value) return; 
+
+  var editValue = value.textContent;
+
+  id.innerHTML = `<input type="text" id="input_${taskId}" value="${editValue}">
+  <button class="btn1" onclick="savetask('${taskId}')">Save</button>`;
+}
+
+
+function savetask(taskId){
+  var inputValue =document.getElementById(`input_${taskId}`).value; 
+  var id = document.getElementById(taskId);
+
+  id.innerHTML = `<span>${inputValue}</span>
+   <button class="btn1" onclick="edittask('${taskId}')">Edit</button>
+  <button class="btn1" onclick="deltask('${taskId}')">Done</button>`;
 }
